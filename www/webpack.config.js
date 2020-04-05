74c5e68b4c9a6dc634b5ad2254352c1597e9a3e0
chengluyu <chengluyu@live.cn>
2020-04-05 12:50:53 +0800

    build: change output path to "public"

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   entry: "./bootstrap.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "public"),
     filename: "bootstrap.js",
   },
   mode: "development",
